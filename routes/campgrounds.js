df1265d704fcf776a9076f1dc822ed4a7b1519d7
ioanzicu <zicu.ioan@mail.ru>
2018-09-12 13:22:18 +0100

    Change coordinates input field argumenst and modify create new campground schema

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -23,12 +23,16 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 	var name = req.body.name;
 	var price = req.body.price;
 	var image = req.body.image;
+	var address = req.body.address;
+	var lat = req.body.latitude;
+	var lng = req.body.longitude;
+	var phone = req.body.phone;
 	var desc = req.body.description;
 	var author = {
 		id: req.user._id,
 		username: req.user.username
 	}
-	var newCampground = {name: name, price:price, image:image, description: desc, author: author}
+	var newCampground = {name: name, price:price, image:image, address:address, latitude:lat, longitude:lng, phone:phone, description:desc, author:author}
 	// Create a new campground and save to Db
 	Campground.create(newCampground, function(err, newlyCreated){
 		if (err) {
